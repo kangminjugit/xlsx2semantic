83e8dff41a2a02bf52a566b88325a6ecd71dd572
kangminjugit <62755858+kangminjugit@users.noreply.github.com>
2026-04-02 21:34:58 +0900

    [FIX] init version 수정

--- xlsx2semantic/__init__.py
+++ xlsx2semantic/__init__.py
@@ -4,5 +4,5 @@ from xlsx2semantic.core import parse, parse_file
 from xlsx2semantic.layout_hint import TableLayoutHint
 from xlsx2semantic.models import ParseResult
 
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __all__ = ["parse", "parse_file", "TableLayoutHint", "ParseResult"]
